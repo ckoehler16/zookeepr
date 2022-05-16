b21fc2d7498192fd5355e0ee853652cc254d617e
Koehler <ckoehler16@gmail.com>
2022-05-16 14:35:07 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { animals } = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.json');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -23,7 +28,7 @@ function filterByQuery(query, animalsArray) {
         personalityTraitsArray.forEach(trait => {
             // Check the trait against each animal in the filteredResults array.
             // Remember this is initially a copy of the animalsArray
-            // Except here we're updating it for each traitin the .forEach() loop
+            // Except here we're updating it for each trait in the .forEach() loop
             // For each trait being targeted by the filter, the filteredResults array will  then contain only the entries that contain that trait
             // At the end we'll have an array of animals that have every one of the traits when the .forEach() loop is finished
             filteredResults = filteredResults.filter(
@@ -52,6 +57,14 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body
+     animalsArray.push(animal);
+
+    // return finished code to post route for response
+    return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -67,6 +80,17 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+// Route that accepts data to be used or stored server-side
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
 });
 
 // said to add to the end of server.js ???
